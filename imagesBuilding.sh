096a941c7e7b09520369a3293591ac456ed1821f
sravanin15 <78396210+sravanin15@users.noreply.github.com>
2021-09-14 18:36:48 +0530

    Delete imagesBuilding.sh

--- imagesBuilding.sh
+++
@@ -1,8 +0,0 @@
-#Building docker image for ui
-docker build -t todoapp-ui -f templates/dockerfile .
-docker tag todoapp-ui gcr.io/myproject1-321806/todoapp_ui
-docker push gcr.io/myproject1-321806/todoapp_ui
-#Building docker image for api
-docker build -t todoapp-api .
-docker tag todoapp-ui gcr.io/myproject1-321806/todoapp_api
-docker push gcr.io/myproject1-321806/todoapp_api
